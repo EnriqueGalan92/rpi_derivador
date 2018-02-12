bfbee0e9591a9715b88b320a82139ad806d1755e
EnriqueGalan92 <enriquegalan92@gmail.com>
2018-02-12 01:49:18 +0000

    Added integrator and subplots

--- derivador_integrador.py
+++ derivador_integrador.py
@@ -26,12 +26,21 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 counter = 0
 
 # Derivator Variable declarations
-dt = 0.1
+dt = 0.001
 T  = 10
 L  = 3 # Gain on robust derivator
 v0 = 0
 z1 = 0
 z0 = 0
+dato_inicio = 0
+dato_mitad = 0
+dato_final = 0
+pot_values = np.arange(0)
+der_values = np.arange(0)
+int_values = np.arange(0)
+time_values = np.arange(0)
+
+f, (ax1, ax2, ax3) = plt.subplots(3, 1)
 
 print('Reading MCP3008 values, press Ctrl-C to quit...')
 # Print nice channel column headers.
@@ -39,18 +48,19 @@ print('Reading MCP3008 values, press Ctrl-C to quit...')
 print('-' * 57)
 print('-' * 20 + 'DERIVADOR ROBUSTO' + '-' * 20)
 print('-' * 57)
+time.sleep(1)
+print('START')
 # Main program loop.
-for j in range (100):
+for j in range (10000):
     # Read all the ADC channel values in a list.
     values = [0]*8
     volt_values = [0]*8
-    pot_values = []
-    der_values = []
     for i in range(8):
         # The read_adc function will get the value of the specified channel (0-7).
         values[i] = mcp.read_adc(i)
         volt_values[i] = adc_volt_value(values[i])
-    pot_values.append(volt_values[5])
+    pot_values=np.append(pot_values,volt_values[5])
+        
     y = volt_values[5]
 
     # Print the ADC values.
@@ -60,14 +70,31 @@ for j in range (100):
     v0 = (-1*pow(L, 0.5)) * pow(abs(z0-y),0.5) * sign_detect(z0-y) + z1
     z1 = z1 + dt * (-1.1 * L * sign_detect(z0-y))
     z0 = z0 + dt * v0
-    der_values.append(z1)
-
-    print ('Valor = {5:>4} '.format(*volt_values))
+    if (j == 0):
+        z1 = 0
+        dato_inicio = y
+    der_values = np.append(der_values,z1)
     counter += 1
     # Pause for half a second.
+
+    # Integral
+    jdt = j * dt
+    dato_inicio = y
+    if(j%10 == 0):
+    	datomitad = y
+    if(j%20 == 0):
+        datofinal = y
+    simpson_int = (jdt/6) * (dato_inicio + 4*dato_mitad + dato_final)
+    int_values = np.append(int_values, simpson_int)
+    time_values = np.append(time_values, jdt)
+                                 
     time.sleep(dt)
 
-ax = plt.subplot(111)
-t = np.arange(0.0, T, dt)
-line, = plt.plot(t, pot_values, lw = 2)
+print('STOP')
+print(pot_values)
+print(der_values)
+
+ax1.plot(pot_values, 'r')
+ax2.plot(der_values, 'b')
+ax3.plot(int_values, 'g')
 plt.show()
